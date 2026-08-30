9459899a3c2fff92a173ededadb44332ac4a441d
Manav Sharma <manav.acno1@gmail.com>
2026-08-30 14:39:03 +0530

    feat: handlerUser, createUser in DB, save request in DB

--- code/backend/internal/database/mongodb.go
+++ code/backend/internal/database/mongodb.go
@@ -16,6 +16,16 @@ type User struct {
 	UpdatedAt time.Time     `bson:"updatedAt" json:"updatedAt"`
 }
 
+// Req represents a request in the MongoDB database.
+type Req struct {
+	ID        bson.ObjectID `bson:"_id,omitempty" json:"id"`
+	Payload   []byte        `bson:"payload" json:"payload"`
+	Status    string        `bson:"status" json:"status"`
+	Attempts  int           `bson:"attempts" json:"attempts"`
+	CreatedAt time.Time     `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time     `bson:"updatedAt" json:"updatedAt"`
+}
+
 // CreateUser creates a new user in the MongoDB database.
 func (m *MongoDB) CreateUser(ctx context.Context, name, email string) (*User, error) {
 	now := time.Now()
@@ -35,4 +45,39 @@ func (m *MongoDB) CreateUser(ctx context.Context, name, email string) (*User, er
 	}
 
 	return user, nil
+}
+
+// CreateReq creates a new request in the MongoDB database.
+func (m *MongoDB) CreateReq(ctx context.Context, payload []byte) (*Req, error) {
+	now := time.Now()
+	req := &Req{
+		ID:        bson.NewObjectID(),
+		Payload:   payload,
+		Status:    "pending",
+		Attempts:  0,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	collection := m.Collections[CollectionRequests]
+
+	_, err := collection.InsertOne(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+// FindUserByEmail finds if a user already exists in the database with given Email
+func (m *MongoDB) FindUserByEmail(ctx context.Context, email string) (*User, error) {
+	collection := m.Collections[CollectionUsers]
+	
+	var user User
+	err := collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	
+	return &user, nil
 }
